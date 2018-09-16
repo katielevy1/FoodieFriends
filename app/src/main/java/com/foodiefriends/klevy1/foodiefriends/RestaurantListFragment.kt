6d09cb4bcd42680da52e8eb68ed89bd787738576
Katie Levy <klevy@sandiego.edu>
2018-09-16 13:07:02 -0700

    adding fragment for restaurant

--- app/src/main/java/com/foodiefriends/klevy1/foodiefriends/RestaurantListFragment.kt
+++ app/src/main/java/com/foodiefriends/klevy1/foodiefriends/RestaurantListFragment.kt
@@ -1,13 +1,60 @@
 package com.foodiefriends.klevy1.foodiefriends
 
+import android.app.Application
 import android.app.Fragment
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.TextView
 
 class RestaurantListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View {
-        return inflater.inflate(R.layout.restaurant_list_fragment, container, false)
+        val viewManager = LinearLayoutManager(activity)
+        val viewAdapter = MyAdapter(arrayOf("place 1", "place 2"))
+        val view = inflater.inflate(R.layout.restaurant_list_fragment, container, false)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.restaurant_list_recycler_view).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+
+        }
+        return view
+
+
     }
+}
+
+class MyAdapter(private val myDataset: Array<String>) :
+        RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): MyAdapter.MyViewHolder {
+        // create a new view
+        val textView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.my_text_view, parent, false) as TextView
+        // set the view's size, margins, paddings and layout parameters
+        return MyViewHolder(textView)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.text = myDataset[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
 }
