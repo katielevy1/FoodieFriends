62b8dc4db2cc9ad917db911fe0cb5b2c85b4adc1
katielevy1 <katie.levy1@gmail.com>
2018-09-23 19:21:42 -0700

    Restaurant list items in as recyclerview

--- app/src/main/java/com/foodiefriends/klevy1/foodiefriends/RestaurantListFragment.kt
+++ app/src/main/java/com/foodiefriends/klevy1/foodiefriends/RestaurantListFragment.kt
@@ -1,6 +1,7 @@
 package com.foodiefriends.klevy1.foodiefriends
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -31,7 +32,7 @@ class MyAdapter(private val myDataset: Array<String>) :
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    class MyViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class MyViewHolder(val view: ConstraintLayout) : RecyclerView.ViewHolder(view)
 
 
     // Create new views (invoked by the layout manager)
@@ -39,7 +40,7 @@ class MyAdapter(private val myDataset: Array<String>) :
                                     viewType: Int): MyAdapter.MyViewHolder {
         // create a new view
         val textView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.my_text_view, parent, false) as TextView
+                .inflate(R.layout.restaurant_item, parent, false) as ConstraintLayout
         // set the view's size, margins, paddings and layout parameters
         return MyViewHolder(textView)
     }
@@ -48,7 +49,7 @@ class MyAdapter(private val myDataset: Array<String>) :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.text = myDataset[position]
+        holder.view.findViewById<TextView>(R.id.restaurant_name).text = myDataset[position]
     }
 
     // Return the size of your dataset (invoked by the layout manager)
